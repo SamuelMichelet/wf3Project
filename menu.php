8800f9860345ac7979414614b692187e42bc8cd0
Akwind <florian.fontaine@gmail.com>
2018-11-05 15:41:46 +0100

    Ajout menu

--- menu.php
+++ menu.php
@@ -1,4 +1,28 @@
 <nav>
-    <a href="index.php">Home</a>
-    <a href="profil.php">Profil</a>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="profil.php">Profil</a></li>
+        <?php
+            if(!isset($_SESSION['account'])){
+        ?>
+        <li><a href="connexion">Connexion</a></li>
+        <?php
+            }
+        ?>
+        <?php
+            if(!isset($_SESSION['account'])){
+        ?>
+        <li><a href="inscription">Inscription</a></li>
+        <?php
+            }
+        ?>
+        <?php
+            if(isset($_SESSION['account'])){
+        ?>
+        <li><a href="deconnexion">Déconnexion</a></li>
+        <?php
+            }
+        ?>
+
+    </ul>
 </nav>
