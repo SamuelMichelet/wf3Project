ae546ef7d6e77e65aa8aaa871ba2da31bbc25f67
Akwind <florian.fontaine@gmail.com>
2018-11-05 10:35:31 +0100

    ajout

--- bdd/users.sql
+++ bdd/users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le :  lun. 05 nov. 2018 à 10:19
+-- Généré le :  lun. 05 nov. 2018 à 10:33
 -- Version du serveur :  10.1.36-MariaDB
 -- Version de PHP :  7.2.10
 
@@ -21,6 +21,8 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `users`
 --
+CREATE DATABASE IF NOT EXISTS `users` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `users`;
 
 -- --------------------------------------------------------
 
